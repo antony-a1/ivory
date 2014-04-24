1b81c1d20349e741dda9e15206119efbc3b6025d
Eric Torreborre <etorreborre@yahoo.com>
2014-04-24 14:56:33 +1000

    using the entities map on the reducer side

--- ivory-chord/src/main/scala/com/ambiata/ivory/chord/Chord.scala
+++ ivory-chord/src/main/scala/com/ambiata/ivory/chord/Chord.scala
@@ -17,11 +17,21 @@ import com.ambiata.ivory.scoobi.ScoobiAction
 import com.ambiata.ivory.storage._
 import com.ambiata.ivory.validate.Validate
 import com.ambiata.ivory.alien.hdfs._
+import DateMap.localDateToInt
 
 case class HdfsChord(repoPath: Path, store: String, dictName: String, entities: Path, errorPath: Path, storer: IvoryScoobiStorer[Fact, DList[_]]) {
   import IvoryStorage._
 
-  type Priority = Short
+  type Priority   = Short
+  type PackedDate = Int
+  // mappings of each entity to an array of target dates, represented as Ints and sorted from more recent to least
+  type Mappings   = HashMap[String, Array[PackedDate]]
+
+  // lexical order for a pair (Fact, Priority) so that
+  // p1 < p2 <==> f1.time > f2.time || f1.time == f2 && priority1 < priority2
+  implicit val latestOrder: Order[LocalDateTime] = DateTimex.LocalDateTimeHasOrder.reverseOrder
+  implicit val ord: Order[(Fact, Priority)] = Order.orderBy { case (f, p) => (f.time, p) }
+
 
   def withStorer(newStorer: IvoryScoobiStorer[Fact, DList[_]]): HdfsChord =
     copy(storer = newStorer)
@@ -35,52 +45,70 @@ case class HdfsChord(repoPath: Path, store: String, dictName: String, entities: 
     _  <- storer.storeMeta
   } yield ()
 
-  def scoobiJob(repo: HdfsRepository, dict: Dictionary, store: FeatureStore, entities: HashMap[String, Array[Int]]): ScoobiAction[Unit] =
+  /**
+   * Persist facts which are the latest corresponding to a set of dates given for each entity
+   */
+  def scoobiJob(repo: HdfsRepository, dict: Dictionary, store: FeatureStore, entities: Mappings): ScoobiAction[Unit] =
     ScoobiAction.scoobiJob({ implicit sc: ScoobiConfiguration =>
       lazy val factsetMap = store.factSets.map(fs => (fs.priority.toShort, fs.name)).toMap
-      factsFromIvoryStore(repo, store).map(input => {
+
+      factsFromIvoryStore(repo, store).map { input =>
         val entityMap = DObject(entities)
 
-        val errors: DList[String] = input.collect {
-          case -\/(e) => e
-        }
+        val errors: DList[String] = input.collect { case -\/(e) => e }
 
-        val facts: DList[(Priority, Fact)] = (entityMap join input).mapFlatten({
-          case (es, \/-((p, _, f))) =>
-            Option(es.get(f.entity)).map(_.head).flatMap(intDate => {
-              if(DateMap.localDateToInt(f.date) <= intDate) Some((p.toShort, f)) else None
-            })
-          case _                    => None
-        })
+        // filter out the facts which are not in the entityMap or
+        // which date are greater than the required dates for this entity
+        val facts: DList[(Priority, Fact)] =
+          (entityMap join input)
+            .collect { case (map, \/-((p, _, fact))) => (map, (p.toShort, fact)) }
+            .filter  { case (map, (p, f)) => Option(map.get(f.entity)).exists(_.headOption.getOrElse(0) >= localDateToInt(f.date)) }
+            .map(_._2)
 
-        /*
+        /**
          * 1. group by entity and feature id
-         * 2. take the minimum fact in the group using fact time then priority to determine order
+         * 2. for a given entity and feature id, get the latest facts, with the lowest priority
          */
-        implicit val revDateOrder: Order[LocalDateTime] = DateTimex.LocalDateTimeHasOrder.reverseOrder
-        val ord: Order[(Priority, Fact)] = Order.orderBy { case (p, f) => (f.time, p) }
-        val latest: DList[(Priority, Fact)] = facts.groupBy { case (p, f) => (f.entity, f.featureId.toString) }
-                                                   .reduceValues(Reduction.minimum(ord))
-                                                   .collect { case (_, (p, f)) if !f.isTombstone => (p, f) }
+        val latest: DList[(Priority, Fact)] =
+          facts
+            .groupBy { case (p, f) => (f.entity, f.featureId.toString) }
+            .mapFlatten { case ((entityId, featureId), fs) =>
+              // the required dates
+              val dates = entities.get(entityId)
 
-        val validated: DList[String \/ Fact] = latest.map({ case (p, f) =>
-          Validate.validateFact(f, dict).disjunction.leftMap(e => e + " - Factset " + factsetMap.get(p).getOrElse("Unknown, priority " + p))
-        })
+              // we traverse all facts and for each required date
+              // we keep the "best" fact which date is just before that date
+              fs.foldLeft(dates.map((_, Short.MinValue, None)): Array[(Int, Short, Option[Fact])]) { case (ds, (priority, fact)) =>
+                val factDate = localDateToInt(fact.date)
 
-        val valErrors = validated.collect {
-          case -\/(e) => e
+                ds.map {
+                  case previous @ (date, p, None)    =>
+                    // we found a first suitable fact for that date
+                    if (factDate <= date) (date, priority, Some(fact))
+                    else                  previous
+
+                  case previous @ (date, p, Some(f)) =>
+                    // we found a fact with a better time, or better priority if there is a tie
+                    if (factDate <= date && (fact, priority) < ((f, p))) (date, priority, Some(fact))
+                    else                                               previous
+                }
+              }.collect { case (d, p, Some(f)) => (p, f) }.toIterable
+            }.collect { case (p, f) if !f.isTombstone => (p, f) }
+
+        val validated: DList[String \/ Fact] = latest.map { case (p, f) =>
+          Validate.validateFact(f, dict).disjunction.leftMap(_ + " - Factset " + factsetMap.get(p).getOrElse("Unknown, priority " + p))
         }
 
-        val good = validated.collect {
-          case \/-(f) => f
-        }
+        val valErrors = validated.collect { case -\/(e) => e }
+        val good      = validated.collect { case \/-(f) => f }
 
-        persist(errors.toTextFile((new Path(errorPath, "parse")).toString),
-                valErrors.toTextFile((new Path(errorPath, "validation")).toString),
-                storer.storeScoobi(good))(sc)
+        persist(errors.toTextFile(new Path(errorPath, "parse").toString),
+                valErrors.toTextFile(new Path(errorPath, "validation").toString),
+                storer.storeScoobi(good))
         ()
-      })
+      }
     }).flatten
+
 }
 
 object Chord {
@@ -91,11 +119,6 @@ object Chord {
   def readChords(path: Path): Hdfs[HashMap[String, Array[Int]]] = for {
     chords <- Hdfs.readWith(path, is => Streams.read(is))
   } yield DateMap.chords(chords)
-
-  def readEntities(path: Path): Hdfs[Map[String, Array[String]]] = for {
-    lines <- Hdfs.readWith(path, is => Streams.read(is)).map(_.lines.toList)
-    map   <- Hdfs.fromDisjunction(parseLines(lines))
-  } yield map
 
   def parseLines(lines: List[String]): String \/ Map[String, Array[String]] =
     lines.traverseU(l => entityParser.run(Delimited.parsePsv(l)).disjunction).map(entries => {
--- ivory-chord/src/test/scala/com/ambiata/ivory/chord/ChordSpec.scala
+++ ivory-chord/src/test/scala/com/ambiata/ivory/chord/ChordSpec.scala
@@ -23,42 +23,52 @@ import IvoryStorage._
 class ChordSpec extends HadoopSpecification with SimpleJobs with FileMatchers {
   override def isCluster = false
 
-
   "Can extract expected facts" >> { implicit sc: ScoobiConfiguration =>
-    implicit val fs = sc.fileSystem
-
     val directory = path(TempFiles.createTempDir("chord").getPath)
     val repo = Repository.fromHdfsPath(new Path(directory + "/repo"))
-    val outpath = directory + "/out"
-    val errpath = directory + "/err"
 
+    createEntitiesFiles(directory)
+    createDictionary(repo)
+    createFacts(repo)
+  
+    val storer = DelimitedFactTextStorage.DelimitedFactTextStorer(new Path(directory+"/out"))
+    Chord.onHdfs(repo.path, "store1", "dict1", new Path(directory+"/entities"), storer.path, new Path(directory+"/err"), storer).run(sc) must beOk
+
+    fromTextFile(storer.path.toString).run.toSet must_==
+    Set("eid1|ns1:fid1|def|2012-09-01 00:00:00", "eid1|ns1:fid1|abc|2012-10-01 00:00:00", "eid2|ns1:fid2|11|2012-11-01 00:00:00")
+  }
+  
+  def createEntitiesFiles(directory: String)(implicit sc: ScoobiConfiguration) = {
+    implicit val fs = sc.fileSystem
     val entities = Seq("eid1|2012-09-15", "eid2|2012-12-01", "eid1|2012-11-01")
 
     lazy val entitiesFile = new File(directory + "/entities")
     TempFiles.writeLines(entitiesFile, entities, isRemote)
-
+  }
+  
+  def createDictionary(repo: HdfsRepository)(implicit sc: ScoobiConfiguration) = {
     val dict = Dictionary("dict1", Map(FeatureId("ns1", "fid1") -> FeatureMeta(StringEncoding, CategoricalType, "desc"),
-                                       FeatureId("ns1", "fid2") -> FeatureMeta(IntEncoding, NumericalType, "desc"),
-                                       FeatureId("ns2", "fid3") -> FeatureMeta(BooleanEncoding, CategoricalType, "desc")))
+      FeatureId("ns1", "fid2") -> FeatureMeta(IntEncoding, NumericalType, "desc"),
+      FeatureId("ns2", "fid3") -> FeatureMeta(BooleanEncoding, CategoricalType, "desc")))
 
     dictionaryToIvory(repo, dict, dict.name).run(sc) must beOk
-
-    val facts1 = DList(StringFact("eid1", FeatureId("ns1", "fid1"), new LocalDate(2012, 10, 1), 0, "abc"),
-                       StringFact("eid1", FeatureId("ns1", "fid1"), new LocalDate(2012, 9, 1), 0, "def"),
-                       IntFact("eid2", FeatureId("ns1", "fid2"), new LocalDate(2012, 10, 1), 0, 10),
-                       IntFact("eid2", FeatureId("ns1", "fid2"), new LocalDate(2012, 11, 1), 0, 11),
-                       BooleanFact("eid3", FeatureId("ns2", "fid3"), new LocalDate(2012, 3, 20), 0, true))
-    val facts2 = DList(StringFact("eid1", FeatureId("ns1", "fid1"), new LocalDate(2012, 9, 1), 0, "ghi"))
+  }
+  
+  def createFacts(repo: HdfsRepository)(implicit sc: ScoobiConfiguration) = {
+    val facts1 =
+      DList(StringFact ("eid1", FeatureId("ns1", "fid1"),  new LocalDate(2012, 10, 1), 0, "abc"),
+            StringFact ("eid1", FeatureId("ns1", "fid1"),  new LocalDate(2012, 9, 1),  0, "def"),
+            IntFact    ("eid2", FeatureId("ns1", "fid2"),  new LocalDate(2012, 10, 1), 0, 10),
+            IntFact    ("eid2", FeatureId("ns1", "fid2"),  new LocalDate(2012, 11, 1), 0, 11),
+            BooleanFact("eid3", FeatureId("ns2", "fid3"),  new LocalDate(2012, 3, 20), 0, true))
+  
+    val facts2 =
+      DList(StringFact("eid1", FeatureId("ns1", "fid1"), new LocalDate(2012, 9, 1), 0, "ghi"))
 
     persist(facts1.toIvoryFactset(repo, "factset1"), facts2.toIvoryFactset(repo, "factset2"))
     writeFactsetVersion(repo, List("factset1", "factset2")).run(sc) must beOk
-    
+
     storeToIvory(repo, FeatureStore(List(FactSet("factset1", 1), FactSet("factset2", 2))), "store1").run(sc) must beOk
 
-    val storer = DelimitedFactTextStorage.DelimitedFactTextStorer(new Path(outpath))
-    Chord.onHdfs(repo.path, "store1", "dict1", new Path(entitiesFile.toString), new Path(outpath), new Path(errpath), storer).run(sc) must beOk
-
-    val res = fromTextFile(outpath).run.toList
-    res must_== List("eid1:2012-09-15|ns1:fid1|def|2012-09-01 00:00:00", "eid1:2012-11-01|ns1:fid1|abc|2012-10-01 00:00:00", "eid2:2012-12-01|ns1:fid2|11|2012-11-01 00:00:00")
-  }.pendingUntilFixed("Reduction needs to be implemented")
+  }
 }
