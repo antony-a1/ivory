68781811dae0194b03384c2f0d96119a36af0052
markhibberd <mark@hibberd.id.au>
2014-05-02 13:18:53 +1000

    Fixed dropping.

--- ivory-core/src/main/scala/com/ambiata/ivory/core/Location.scala
+++ ivory-core/src/main/scala/com/ambiata/ivory/core/Location.scala
@@ -3,7 +3,7 @@ package com.ambiata.ivory.core
 import scalaz._, Scalaz._
 import org.apache.hadoop.fs.Path
 import com.ambiata.mundane.io.FilePath
--
+
 sealed trait Location
 case class HdfsLocation(path: String) extends Location
 case class S3Location(bucket: String, path: String) extends Location
