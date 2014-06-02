76bc418b7b4c264fb3785ffd3a169bb377dbb1e3
markhibberd <mark@hibberd.id.au>
2014-06-02 11:43:56 +1000

    No compression option.

--- ivory-cli/src/main/scala/com/ambiata/ivory/cli/ingestBulk.scala
+++ ivory-cli/src/main/scala/com/ambiata/ivory/cli/ingestBulk.scala
@@ -21,7 +21,7 @@ object ingestBulk extends ScoobiApp {
 
   val tombstone = List("☠")
 
-  case class CliArguments(repo: String, dictionary: Option[String], input: String, tmp: String, timezone: DateTimeZone)
+  case class CliArguments(repo: String, dictionary: Option[String], input: String, tmp: String, timezone: DateTimeZone, codec: Option[CompressionCodec])
 
   val parser = new scopt.OptionParser[CliArguments]("ingest") {
     head("""
@@ -32,6 +32,7 @@ object ingestBulk extends ScoobiApp {
          |""".stripMargin)
 
     help("help") text "shows this usage text"
+    opt[String]('n', "no-compression") action { (x, c) => c.copy(codec = None) }       required() text "Don't use compression."
     opt[String]('r', "repo") action { (x, c) => c.copy(repo = x) }       required() text "Path to an ivory repository."
     opt[String]('t', "tmp")        action { (x, c) => c.copy(tmp = x) }        required() text "Path to store tmp data."
     opt[String]('i', "input")      action { (x, c) => c.copy(input = x) }      required() text "Path to data to import."
@@ -45,8 +46,8 @@ object ingestBulk extends ScoobiApp {
   type Parts = String
 
   def run {
-    parser.parse(args, CliArguments("", None, "", "", DateTimeZone.getDefault)).map(c => {
-      val res = onHdfs(new Path(c.repo), c.dictionary, new Path(c.input), tombstone, new Path(c.tmp), c.timezone)
+    parser.parse(args, CliArguments("", None, "", "", DateTimeZone.getDefault, Some(new SnappyCodec))).map(c => {
+      val res = onHdfs(new Path(c.repo), c.dictionary, new Path(c.input), tombstone, new Path(c.tmp), c.timezone, c.codec)
       res.run(configuration.modeIs(com.nicta.scoobi.core.Mode.Cluster)).run.unsafePerformIO() match {
         case Ok(_)    => println(s"Successfully imported '${c.input}' into '${c.repo}'")
         case Error(e) => println(s"Failed! - ${Result.asString(e)}")
@@ -54,19 +55,19 @@ object ingestBulk extends ScoobiApp {
     })
   }
 
-  def onHdfs(repo: Path, dictionary: Option[String], input: Path, tombstone: List[String], tmp: Path, timezone: DateTimeZone): ScoobiAction[String] =
-    fatrepo.ImportWorkflow.onHdfs(repo, dictionary.map(defaultDictionaryImport(_)), importFeed(input), tombstone, tmp, timezone)
+  def onHdfs(repo: Path, dictionary: Option[String], input: Path, tombstone: List[String], tmp: Path, timezone: DateTimeZone, codec: Option[CompressionCodec]): ScoobiAction[String] =
+    fatrepo.ImportWorkflow.onHdfs(repo, dictionary.map(defaultDictionaryImport(_)), importFeed(input, codec), tombstone, tmp, timezone)
 
   def defaultDictionaryImport(dictionary: String)(repo: HdfsRepository, name: String, tombstone: List[String], tmpPath: Path): Hdfs[Unit] =
     DictionaryImporter.onHdfs(repo.root.toHdfs, repo.dictionaryByName(dictionary).toHdfs, name)
 
-  def importFeed(input: Path)(repo: HdfsRepository, factset: Factset, dname: String, tmpPath: Path, errorPath: Path, timezone: DateTimeZone): ScoobiAction[Unit] = for {
+  def importFeed(input: Path, codec: Option[CompressionCodec])(repo: HdfsRepository, factset: Factset, dname: String, tmpPath: Path, errorPath: Path, timezone: DateTimeZone): ScoobiAction[Unit] = for {
     dict <- ScoobiAction.fromHdfs(IvoryStorage.dictionaryFromIvory(repo, dname))
     list <- listing(input)
     _    = list.foreach({ case (n, d) => { println(s"namespace[$n] --"); d.foreach(x => println(s"  $x")) }})
     _    = partitions(list).foreach({ case (n, d) => println(s"part $n / $d") })
     conf <- ScoobiAction.scoobiConfiguration
-    _    <- EavtTextImporter.onHdfsBulk(repo, dict, factset, list.map(_._1), input, errorPath, timezone, partitions(list), Some(new SnappyCodec))
+    _    <- EavtTextImporter.onHdfsBulk(repo, dict, factset, list.map(_._1), input, errorPath, timezone, partitions(list), codec)
   } yield ()
 
   def partitions(ns: List[(Namespace, List[Parts])]): Map[String, Int] =
